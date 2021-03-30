7d73958dad543c5189e03816766af0b529ecdaf6
unknown <sinumsd98@gmail.com>
2021-03-31 00:26:41 +0530

    ADDED: Won or tie Added

--- Stimulator.sh
+++ Stimulator.sh
@@ -30,3 +30,44 @@ done
 echo "head won="$countHead
 echo "tail won="$countTail
 
+declare -A noOfCounts
+for((i=0;i<flip;i++))
+target_count=21
+minimum_difference=0
+heads_count=0
+tails_count=0
+flips_count=0
+while [ 1 ]
+do
+if [ $((RANDOM%2)) -eq 1 ]
+toss=$(( RANDOM % 2  ))
+if(( toss == 0 ))
+then
+((countHead++))
+echo "Heads"
+(( heads_count++ ))
+
+else
+((countTail++))
+echo "Tails"
+(( tails_count++ ))
+
+fi
+
+
+diff=$(( heads_count - tails_count ))
+if [[ $heads_count -eq $target_count  ||  $tails_count -eq $target_count ]] && [ $diff -eq 0 ];then
+echo "Tie "
+break
+
+elif [ $heads_count -eq $target_count ] && [ $diff -gt $minimum_difference ];then
+        echo "Heads won by $diff"
+        break
+elif [ $tails_count -eq $target_count ] && [ $diff -gt $minimum_difference ];then
+         echo "Tails won by ${diff} points"
+         break
+fi
+
+done
+echo "head won="$countHead
+echo "tail won="$countTai
